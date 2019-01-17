019266ee4c31d9b3dd446ff2b4ebf35cb16c5a9e
vpinnaka <vinayindia2020@gmail.com>
2019-01-17 01:51:00 -0600

    Updated the doctor appointments

--- drchrono/models.py
+++ drchrono/models.py
@@ -1,5 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Add your models here
 
 
+class AppointmentSchedule(models.Model):
+    patient = models.CharField(max_length=50)
+    appointment_time = models.DateTimeField(default=timezone.now)
+    appointment_status = models.CharField(max_length=50)
+    checkedin_status = models.BooleanField(default=False)
+    checkedin_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '{} appointment at {}'.format(self.patient, self.appointment_time)
--- drchrono/views.py
+++ drchrono/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from social_django.models import UserSocialAuth
-
-from drchrono.endpoints import DoctorEndpoint
+from django.utils import timezone
+import datetime
+from drchrono.endpoints import DoctorEndpoint, AppointmentEndpoint
 
 
 class SetupView(TemplateView):
@@ -37,13 +38,14 @@ class DoctorWelcome(TemplateView):
         api = DoctorEndpoint(access_token)
         # Grab the first doctor from the list; normally this would be the whole practice group, but your hackathon
         # account probably only has one doctor in it.
-        return next(api.list())
+        appointments = AppointmentEndpoint(access_token)
+        return (next(api.list()), appointments.list(date=datetime.datetime.now()))
 
     def get_context_data(self, **kwargs):
         kwargs = super(DoctorWelcome, self).get_context_data(**kwargs)
         # Hit the API using one of the endpoints just to prove that we can
         # If this works, then your oAuth setup is working correctly.
-        doctor_details = self.make_api_request()
+        doctor_details, appointments = self.make_api_request()
         kwargs['doctor'] = doctor_details
+        kwargs['appointments'] = list(appointments)
         return kwargs
-
